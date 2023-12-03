3fed3e44c7163fe2b7b12951aa5eb20092da531c
Novita H <novitahamilton@gmail.com>
2023-12-02 21:02:59 -0800

    Step 2: Creating a Child Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,17 @@
 import '../styles/App.css';
+import BookList from './BookList';
 
 function App() {
   return (
     <div className="App">
       <h1>My Reads</h1>
-      <p>Add book titles that you would recommend others to read. 😊</p>
+      <p>
+        Add book titles that you have read and would recommend others to read.
+        😊
+      </p>
+      <div className="book-list">
+        <BookList />
+      </div>
     </div>
   );
 }
---
+++ src/components/BookList.js
@@ -0,0 +1,11 @@
+function BookList({ books }) {
+  return (
+    <ul>
+      <li>Kite Runner</li>
+      <li>The House in the Cerulean Sea Book by TJ Klune</li>
+      <li>The Handmaid's Tail</li>
+    </ul>
+  );
+}
+
+export default BookList;
