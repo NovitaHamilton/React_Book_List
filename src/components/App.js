3f83fbf94d58b6423d812a7428f51e017193ffc6
Novita H <novitahamilton@gmail.com>
2023-12-02 20:06:29 -0800

    Step 1: Initial Setup

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Reads</h1>
+      <p>Add book titles that you would recommend others to read. 😊</p>
+    </div>
+  );
+}
+
+export default App;
